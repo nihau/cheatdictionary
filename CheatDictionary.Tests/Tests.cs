0ecc16af0fc93864b43e6f9652ee7eee9d15ba8f
Gorbach Nikita <gorbach@ACTIMIND>
2016-02-19 18:19:11 +0300

    initial

---
+++ CheatDictionary.Tests/Tests.cs
@@ -0,0 +1,137 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CheatDictionary;
+
+namespace CheatDictionary.Tests
+{   
+    [TestFixture]
+    public class Tests
+    {
+        public CheatDictionary<string, string> cDic { get; set; }
+
+        [SetUp]
+        public void SetUp()
+        {
+            cDic = new CheatDictionary<string, string>();
+        }
+
+        [Test]
+        public void Add_KeyExists()
+        {
+            const string key = "123";
+            const string value = "321";
+
+            cDic.Add(key, value);
+
+            Assert.IsTrue(cDic.ContainsKey(key));
+        }
+
+        [Test]
+        public void Add_IndexerValueSame()
+        {
+            const string key = "123";
+            const string value = "321";
+
+            cDic.Add(key, value);
+
+            Assert.AreEqual(cDic[key], value);
+        }
+
+        [Test]
+        public void Add_TryGetValueSame()
+        {
+            const string key = "123";
+            const string value = "321";
+            string outValue = null;
+
+            cDic.Add(key, value);
+
+            Assert.IsTrue(cDic.TryGetValue(key, out outValue));
+            Assert.AreEqual(outValue, value);
+        }
+
+        [Test]
+        public void AddSameKeys_NoException()
+        {
+            const string key = "123";
+            const string value1 = "321";
+            const string value2 = "321321";
+
+            cDic.Add(key, value1);
+            cDic.Add(key, value2);
+        }
+
+        [Test]
+        public void AddSameKeys_ForeachContainsBoth()
+        {
+            const string key = "123";
+            const string value1 = "321";
+            const string value2 = "321321";
+
+            cDic.Add(key, value1);
+            cDic.Add(key, value2);
+
+            var l = cDic.ToList();
+
+            Assert.AreEqual(l[0].Key, key);
+            Assert.AreEqual(l[1].Key, key);
+
+            Assert.AreEqual(l[0].Value, value1);
+            Assert.AreEqual(l[1].Value, value2);
+        }
+
+        [Test]
+        public void AddKeyValuePair_KeyExists()
+        {
+            const string key = "123";
+            const string value = "321";
+
+            cDic.Add(new KeyValuePair<string, string>(key, value));
+
+            Assert.IsTrue(cDic.ContainsKey(key));
+        }
+
+        [Test]
+        public void AddKeyValuePair_KvpExists()
+        {
+            const string key = "123";
+            const string value = "321";
+
+            var kvp = new KeyValuePair<string, string>(key, value);
+
+            cDic.Add(new KeyValuePair<string, string>(key, value));
+
+            Assert.IsTrue(cDic.Contains(kvp));
+        }
+
+        [Test]
+        public void Add100_CountEquals100()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                cDic.Add(i.ToString(), (i * 2 + 1).ToString());
+            }
+
+            Assert.AreEqual(cDic.Count, 100);
+        }
+
+        [Test]
+        public void Add100_Clear_CountEquals0()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                cDic.Add(i.ToString(), (i * 2 + 1).ToString());
+            }
+
+            Assert.AreEqual(cDic.Count, 100);
+
+            cDic.Clear();
+
+            Assert.AreEqual(cDic.Count, 0);
+        }
+    }
+}
---
+++ CheatDictionary/CheatDictionary.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheatDictionary
+{
+    /// <summary>
+    /// allows to contain same key multiple times
+    /// </summary>
+    public class CheatDictionary<K, V> : IDictionary<K, V>
+    {
+        private List<Dictionary<K, V>> _dictionaries;
+        public IEqualityComparer<K> Comparer { get; set; }
+
+        public ICollection<K> Keys
+        {
+            get
+            {
+                var l = new List<K>();
+
+                foreach (var dic in _dictionaries)
+                {
+                    l.AddRange(dic.Keys);
+                }
+
+                return l;
+            }
+        }
+
+        public ICollection<V> Values
+        {
+            get
+            {
+                var l = new List<V>();
+
+                foreach (var dic in _dictionaries)
+                {
+                    l.AddRange(dic.Values);
+                }
+
+                return l;
+            }
+        }
+
+        public V this[K key]
+        {
+            get
+            {
+                foreach (var dic in _dictionaries)
+                {
+                    try
+                    {
+                        return dic[key];
+                    }
+                    catch (Exception)
+                    {
+
+                    }
+                }
+
+                throw new KeyNotFoundException();
+            }
+            set
+            {
+                foreach (var dic in _dictionaries)
+                {
+                    try
+                    {
+                        dic[key] = value;
+
+                        return;
+                    }
+                    catch (Exception)
+                    {
+
+                    }
+                }
+            }
+        }
+
+        public CheatDictionary(IEqualityComparer<K> comparer)
+        {
+            Comparer = comparer;
+
+            _dictionaries = new List<Dictionary<K, V>>();
+
+            AddNewDictionary();
+        }
+
+        public CheatDictionary()
+            : this(EqualityComparer<K>.Default)
+        {
+        }
+
+        private void AddNewDictionary()
+        {
+            _dictionaries.Add(new Dictionary<K, V>(Comparer));
+        }
+
+        public void Add(K key, V value)
+        {
+            foreach (var dic in _dictionaries)
+            {
+                if (!dic.ContainsKey(key))
+                {
+                    dic.Add(key, value);
+                    return;
+                }
+            }
+
+            AddNewDictionary();
+            _dictionaries.Last().Add(key, value);
+        }
+
+        public bool ContainsKey(K key)
+        {
+            foreach (var dic in _dictionaries)
+            {
+                if (dic.ContainsKey(key))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public bool Remove(K key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool TryGetValue(K key, out V value)
+        {
+            foreach (var dic in _dictionaries)
+            {
+                if (dic.ContainsKey(key))
+                {
+                    value = dic[key];
+                    return true;
+                }
+            }
+
+            value = default(V);
+            return false;
+        }
+
+        public void Add(KeyValuePair<K, V> item)
+        {
+            Add(item.Key, item.Value);
+        }
+
+        public void Clear()
+        {
+            _dictionaries.Clear();
+        }
+
+        public bool Contains(KeyValuePair<K, V> item)
+        {
+            foreach (var dic in _dictionaries)
+            {
+                if (((ICollection<KeyValuePair<K, V>>)dic).Contains(item))
+                    return true;
+            }
+
+            return false;
+        }
+
+        public void CopyTo(KeyValuePair<K, V>[] array, int arrayIndex)
+        {
+            var l = new List<KeyValuePair<K, V>>();
+
+            l.AddRange(_dictionaries.SelectMany(u => u.ToList()));
+
+            l.CopyTo(array, arrayIndex);
+        }
+
+        public int Count
+        {
+            get { return _dictionaries.Select(u => u.Count).Sum(); }
+        }
+
+        public bool IsReadOnly
+        {
+            get { return false; }
+        }
+
+        public bool Remove(KeyValuePair<K, V> item)
+        {
+            foreach (var dic in _dictionaries)
+            {
+                var col = (ICollection<KeyValuePair<K, V>>)dic;
+
+                if (col.Contains(item))
+                {
+                    return col.Remove(item);
+                }
+            }
+
+            return false;
+        }
+
+        public IEnumerator<KeyValuePair<K, V>> GetEnumerator()
+        {
+            foreach (var dic in _dictionaries)
+            {
+                foreach (var kvp in dic)
+                    yield return kvp;
+            }
+        }
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+}
